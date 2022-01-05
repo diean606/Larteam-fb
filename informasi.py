8c56fa553f99b9c15d2b936af835a30d35b77476
diean606 <97112745+diean606@users.noreply.github.com>
2022-01-06 00:47:07 +0700

    Delete informasi.py

--- informasi.py
+++
@@ -1,9 +0,0 @@
-from modul import *
-
-def generate(cookie,html,username=None):
-	for x in html.find_all("a",href=True):
-		if "/friends?" in x["href"]:
-			if "Teman" in x.text:
-				username=re.search("\/(.*?)\/friends\?",x["href"]).group(1)
-	uid=re.search("c_user=(\d*)",cookie).group(1)
-	open("cookies/info.txt","w").write(json.dumps({"uid":uid,"nama":html.find("title").text,"username":username,"cookies":{"cookie":cookie}}))
